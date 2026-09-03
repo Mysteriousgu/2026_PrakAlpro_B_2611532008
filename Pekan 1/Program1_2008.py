9e72ec410c1b5d0ba1f4c66ff9c8149f0fb490e7
mysteriousgu <rafifirdaus204@gmail.com>
2026-09-03 10:28:30 +0700

    praktikum alpro 2611532008

---
+++ Pekan 1/Program1_2008.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+Dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print ("Hello world")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print(" / \\ // \\\\ /// \\\\\ ")
+print ("Program ini " "Dibuat oleh a\n"
+       "Muhammad Rafi Firdaus 2611532008")
