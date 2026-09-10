59deb06516ee37509c3bec535a06fefbf5d2c1a8
mysteriousgu <rafifirdaus204@gmail.com>
2026-09-10 09:02:57 +0700

    save

---
+++ Pekan 2/Numerik_2611532008.py
@@ -0,0 +1,13 @@
+#Buat file dengan nama Numerik_2611532008.py
+#create a variable with integer value.
+a=100
+print ("The type of variable having value", a, " is ", type(int(a)))
+
+#create a variable with a float value.
+b=10.2345
+print ("The type of variable having value", b, " is ", type (float(b)))
+
+#create a variable with a complex value.
+c=100+3j
+print ("The type of variable having value", c, " is ", type (complex(c)))
+
