39f9f0ecf88af08eafae89121440311e4412ea9d
mysteriousgu <rafifirdaus204@gmail.com>
2026-09-10 09:05:04 +0700

    praktikum alpro 2611532008

--- Pekan 2/Numerik_2611532008.py
+++ Pekan 2/Numerik_2611532008.py
@@ -1,13 +1,13 @@
 #Buat file dengan nama Numerik_2611532008.py
 #create a variable with integer value.
-a=100
-print ("The type of variable having value", a, " is ", type(int(a)))
+a_2008=100
+print ("The type of variable having value", a_2008, " is ", type(a_2008))
 
 #create a variable with a float value.
-b=10.2345
-print ("The type of variable having value", b, " is ", type (float(b)))
+b_2008=10.2345
+print ("The type of variable having value", b_2008, " is ", type (b_2008))
 
 #create a variable with a complex value.
-c=100+3j
-print ("The type of variable having value", c, " is ", type (complex(c)))
+c_2008=100+3j
+print ("The type of variable having value", c_2008, " is ", type (c_2008))
 
---
+++ Pekan 2/String_2611532008.py
@@ -0,0 +1,15 @@
+#buat file dengan Nama String_2611532008.py
+#nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
+#ganti programnya dan data masing2
+nama_2008 = "Rafi"
+jenis_kelamin_2008 = "L"
+alamat_2008 = """   
+   Gunung Pangilun
+   Kec.Padang Utara
+   Kota Padang
+"""
+agama_2008 = "Islam"
+print ("Nama saya adalah: ", nama_2008)
+print ("jenis kelamin saya adalah: ", jenis_kelamin_2008)
+print ("Alamat saya adalah: ", alamat_2008)
+print ("agama saya adalah: ", agama_2008)
