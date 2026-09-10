e012c8277d15e46030bc30929cb1d239e34e7324
mysteriousgu <rafifirdaus204@gmail.com>
2026-09-10 09:31:59 +0700

    praktikum alpro 2611532008

---
+++ Pekan 2/Input_2611532008.py
@@ -0,0 +1,10 @@
+# Buat file dengan nama Input_2611532008.py
+# Buat program untuk menghitung luas persegi panjang
+# Nama variabel ditambah 4 digit nim terakhir contoh: panjang_1234
+# Program ini menggunakan fungsi input() untuk meminta pengguna
+# memasukan nilai panjang dan lebar persegi panjang.    
+# Nilai yang dimasukkan akan dikonversi menjadi tipe data integer
+panjang_2008 = input('Masukan nilai panjang: ')
+lebar_2008 = input('Masukan nilai lebar: ')
+luas_2008 = int(panjang_2008) * int(lebar_2008)
+print ("Luas =", luas_2008)
